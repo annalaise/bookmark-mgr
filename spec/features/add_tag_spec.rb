e59057e246cf59caa3476a7415d776343410eb9e
Graham Cottridge <codinggc@gmail.com>
2017-07-26 14:17:28 +0100

    Add tag feature test

---
+++ spec/features/add_tag_spec.rb
@@ -0,0 +1,11 @@
+feature 'Adding tag' do
+  scenario 'Add a tag to a link' do
+    visit('/links/new')
+    fill_in 'title', with: 'Sasso Carta Forbici'
+    fill_in 'url', with: 'https://sasso-carta-forbici.herokuapp.com'
+    fill_in 'tags', with: 'education'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include ('education')
+  end
+end
